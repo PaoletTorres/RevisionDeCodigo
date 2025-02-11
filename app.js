3a11a36073998105257b2ac86a9d9b26b00efdff
PaoletTorres <paolet.torrestorres@gmail.com>
2025-02-10 20:39:29 -0600

    correccion json

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ const $n = document.querySelector('name');
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) { // se agrega async ya que se esta usando await 
+  try{ // se agrega un bloque try-catch para poder capturar el error que se lanza con throw new Error
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const response = await fetch(`${usersEndpoint}/${username}`); 
+   if(!person.ok) throw new Error (`Usario no encontrado`); // se agrega esta instrucción es para lanzar un error de forma intencional 
+      const data = await 
+      response.json(); // se agrega esta instrucción porque el método fetch utiliza una solicitud HTTP, donde devuelve un objeto response 
+      console.log(data);
+      $n.textContent = '${data.name}';
+      $b.textContent = '${data.blog}';
+      $l.textContent = '${data.location}';
+} catch (err){ //se agrega catch pprque el error será capturado en la función par amostrar el mensaje en consola 
+    handleError(err); 
 }
 
 function handleError(err) {
