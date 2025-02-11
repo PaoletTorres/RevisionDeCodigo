947a9a06d1de6463dd6e52f37200d260d17f8951
PaoletTorres <paolet.torrestorres@gmail.com>
2025-02-10 20:54:25 -0600

    message commit

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const $l = document.querySelector('.location');
 
 async function displayUser(username) { // se agrega async ya que se esta usando await 
   try{ // se agrega un bloque try-catch para poder capturar el error que se lanza con throw new Error
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`); 
-   if(!person.ok) throw new Error (`Usario no encontrado`); // se agrega esta instrucción es para lanzar un error de forma intencional 
+     $n.textContent = 'cargando...';
+     const response = await fetch(`${usersEndpoint}/${username}`); 
+   if(!response.ok) throw new Error (`Usario no encontrado`); // se agrega esta instrucción es para lanzar un error de forma intencional 
       const data = await 
       response.json(); // se agrega esta instrucción porque el método fetch utiliza una solicitud HTTP, donde devuelve un objeto response 
       console.log(data);
@@ -22,7 +22,7 @@ async function displayUser(username) { // se agrega async ya que se esta usando 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  n.textContent = `Algo salió mal: ${err.message}`; // faltaba agregar .message 
 }
 
 displayUser('stolinski').catch(handleError);
